1c07c0a9b20b8ef596c3bdd21fbbde58a0874c2d
plbtt <tumaryeskermes@gmail.com>
2025-12-02 01:18:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function openTG() {
                     <ol style="text-align: left; padding-left: 18px; margin: 10px 0;">
                         <li>
                             Нажмите 
-                            <svg class="tiktok-dots" width="36" height="22" viewBox="0 0 36 24"> 
+                            <svg class="tiktok-dots" width="36" height="22" viewBox="0 0 36 24" 
                                  style="vertical-align: middle; margin-left: 4px;">
                                 <circle cx="4" cy="12" r="3.4" style="fill:white"></circle>
                                 <circle cx="18" cy="12" r="3.4" style="fill:white"></circle>
@@ -40,5 +40,6 @@ function copyLink() {
     .then(() => { alert('Ссылка скопирована! Вставьте в браузер и откройте.'); })
     .catch(() => { alert('Не удалось скопировать ссылку. Попробуйте вручную.'); });
 }
+
 
 
